baf6fa61b639928cf9e27b184b593dd6ccb1372a
Manasarora1234 <70796207+Manasarora1234@users.noreply.github.com>
2021-12-04 16:41:24 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ window.onload = function() {
   const message = document.getElementById("message")
   document.getElementById("button").addEventListener("click", () => {
     console.log("hello", message)
-    fetch("/.netlify/functions/hello-world?name=" + "Netlify")
+    fetch("/.netlify/functions/hello-world?name=" + "Manas")
       .then((data) => data.json())
       .then(({ msg }) => console.log(msg) || (message.innerHTML = msg))
   })
